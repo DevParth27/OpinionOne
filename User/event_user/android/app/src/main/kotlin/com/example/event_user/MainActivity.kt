65e8e4a5bd402e98fb5698e0ff68d015181b9312
ViberAshes <parth.upadhye.4@gmail.com>
2024-04-24 21:33:23 +0530

    newo

--- User/event_user/android/app/src/main/kotlin/com/example/event_user/MainActivity.kt
+++ User/event_user/android/app/src/main/kotlin/com/example/event_user/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.event_user
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterFragmentActivity() {
 }
--- User/event_user/lib/pages/homepage.dart
+++ User/event_user/lib/pages/homepage.dart
@@ -2,7 +2,7 @@ import 'package:event_user/pages/UserPage.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   HomePageState createState() => HomePageState();
